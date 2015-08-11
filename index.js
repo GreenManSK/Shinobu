7f66fbb02023faa24794d6448f8f17bff4604248
GreenManSK <lukas.kurcik@gmail.com>
2015-08-11 20:56:26 +0200

    Comandline added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var config = require("./libs/Config")("config.json");
 var context = require("./libs/Context")(config);
 
+context.addService("cmd", "libs/Cmd");
+context.addLib("intercept-stdout", "intercept-stdout");
+
+context.getService("cmd").start();
---
+++ libs/Cmd.js
@@ -0,0 +1,105 @@
+var util = require('util');
+var Cmd = function (config, context) {
+    if (!(this instanceof Cmd)) {
+        return new Cmd(config, context);
+    }
+
+    this.config = config;
+    this.context = context;
+    this._addCommands();
+};
+Cmd.prototype.history = [];
+Cmd.prototype.commands = {};
+Cmd.prototype.help = {};
+Cmd.prototype._addCommands = function () {
+    this.addCmd(['help', '?', 'stop', 'close'], [this, this.showHelp], null, 'Shows list of commands');
+    this.addCmd(['exit', 'end', 'stop', 'close'], process.exit, null, 'Stops program');
+};
+Cmd.prototype.addCmd = function (names, cb, params, description) {
+    if (!util.isArray(names)) {
+        names = [names];
+    }
+
+    var namesString = '';
+    for (var i in names) {
+        this.commands[names[i]] = cb;
+        if (i > 0) {
+            namesString += ', ' + names[i];
+        } else {
+            namesString = names[i];
+        }
+    }
+
+    this.help[namesString] = [params, description];
+};
+Cmd.prototype.showHelp = function () {
+    console.log('-------Help-------');
+    for (var cmd in this.help) {
+        console.log(cmd + (this.help[cmd][0] ? ' | ' + this.help[cmd][0] : '') + ' - ' + this.help[cmd][1]);
+    }
+
+    console.log('------------------');
+};
+Cmd.prototype.start = function () {
+    var self = this;
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('data', function (data) {
+        self._addToHistory(data);
+        self.parse(data);
+    });
+    this.context.getLib("intercept-stdout")(function (data) {
+        self._addToHistory(data);
+    });
+};
+Cmd.prototype.parse = function (data) {
+    data = data.toString().trim().match(/([^\s]+)/g);
+    if (data.length > 0) {
+        var command = data[0].toLowerCase(), args = [];
+        for (var i = 1; i < data.length; i++) {
+            args.push(this.cleanArg(data[i]));
+        }
+
+        if (typeof this.commands[command] === 'undefined') {
+            console.log('Unknow command, use help for list of commands');
+        } else {
+            var cb = this.commands[command];
+            if (util.isArray(cb)) {
+                cb[1].apply(cb[0], args);
+            } else {
+                cb.apply(null, args);
+            }
+        }
+    }
+};
+Cmd.prototype.cleanArg = function (arg) {
+    if (typeof arg === 'undefined')
+        return null;
+    arg = arg.trim();
+    switch (arg[0]) {
+        case "'":
+            arg = arg.replace(/'/g, '');
+            break;
+        case '"':
+            arg = arg.replace(/"/g, '');
+            break;
+    }
+    return arg;
+};
+Cmd.prototype._addToHistory = function (data) {
+    this.history.push(data.toString().trim());
+    var maxLength = this.config.get('cmd.maxLength', 50);
+    if (this.history.length > maxLength) {
+        this.history.slice(this.history.length - maxLength, this.history.length);
+    }
+};
+Cmd.prototype.getHistory = function (length) {
+    if (length === undefined)
+        return this.history;
+    var begin = 0, end = this.history.length - 1;
+    if (length < this.history.length) {
+        begin = this.history.length - length - 1;
+    }
+
+    return this.history.slice(begin, end);
+};
+module.exports = Cmd;
