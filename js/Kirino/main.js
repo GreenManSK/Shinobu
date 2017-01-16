41b20bdd6e6d5bd6474f5dcc3107dc38e3255b7d
Lukáš Kurčík <lukas.kurcik@gmail.com>
2017-01-16 21:05:30 +0100

    Added RequireJS for classes in different files

---
+++ js/Kirino/main.js
@@ -0,0 +1,13 @@
+//RequireJs Configuration
+requirejs.config({
+	baseUrl: 'js' // Require starts in js directory
+});
+
+define(function (require) {
+    "use strict";
+	
+	// Requires
+	
+	// Start
+	console.log("Hello");
+});
