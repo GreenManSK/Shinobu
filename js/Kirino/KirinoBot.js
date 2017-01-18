01faa71f75b1d0bee51e036ee3cba6673576a867
Lukáš Kurčík <lukas.kurcik@gmail.com>
2017-01-18 11:44:00 +0100

    KirinoBot for console

---
+++ js/Kirino/KirinoBot.js
@@ -0,0 +1,21 @@
+//namespace Kirino
+define(function (require) {
+    var oldLog = console.log;
+    
+    var FONT_SIZE = "15px";
+    var BG_COLOR = "#000;"
+    var KIRINO_COLOR = "orange";
+    var TEXT_COLOR = "#fff";
+    
+    class KirinoBot {
+        static say(text) {
+            text = "%c " + text;
+            oldLog("%c Kirino: " + text, "color: "+KIRINO_COLOR+"; background-color: "+BG_COLOR+"; font-size: " + FONT_SIZE, "color: "+TEXT_COLOR+"; background-color: "+BG_COLOR+"; font-size: " + FONT_SIZE);
+        }
+    };
+    
+    console.log = KirinoBot.say;
+    console.kirino = KirinoBot.say;
+    
+    return KirinoBot;
+});
