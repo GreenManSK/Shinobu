2bf9827373b646e1cbefaf040742181dd3fb2633
Lukáš Kurčík <lukas.kurcik@gmail.com>
2019-08-18 15:12:46 +0200

    Tile CRUD

--- src/app/shinobu/components/quick-access/quick-access.component.ts
+++ src/app/shinobu/components/quick-access/quick-access.component.ts
@@ -3,6 +3,7 @@ import { Tab } from '../../types/tab';
 import { ChromeMockStorageService } from '../../../mocks/chrome-mock-storage.service';
 import { TabService } from '../../services/tab.service';
 import { Tile } from "../../types/tile";
+import { ShContextMenuClickEvent } from "../../../../../node_modules/ng2-right-click-menu/lib/sh-context-menu.models";
 
 @Component({
   selector: 'quick-access',
@@ -28,9 +29,25 @@ export class QuickAccessComponent implements OnInit {
   ngOnInit() {
   }
 
-  public addNewTile(event: MouseEvent): void {
-    event.preventDefault();
+  public addNewTile( event: MouseEvent = null ): void {
+    if (event) {
+      event.preventDefault();
+    }
     this.activeTile = new Tile('', '', '');
     this.showModal = true;
+  }
+
+  public editTile( event: ShContextMenuClickEvent ): void {
+    this.activeTile = event.data as Tile;
+    this.showModal = true;
+  }
+
+  public deleteTile( event: ShContextMenuClickEvent ): void {
+    const tile = event.data as Tile;
+    const index = this.tab.tiles.indexOf(tile, 0);
+    if (index > -1) {
+      this.tab.tiles.splice(index, 1);
+    }
+    this.tabService.save(this.tab);
   }
 }
