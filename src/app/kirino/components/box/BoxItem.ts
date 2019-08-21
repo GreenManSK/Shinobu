b76351479bd381bcb8d63a39609b2fc8b5019686
Lukáš Kurčík <lukas.kurcik@gmail.com>
2019-08-21 16:45:44 +0200

    Added parent key to BoxItem

--- src/app/kirino/components/box/BoxItem.ts
+++ src/app/kirino/components/box/BoxItem.ts
@@ -5,6 +5,7 @@ export class BoxItem {
     public title: string,
     public text: string,
     public date: Date,
+    public parentKey: any = null,
     public icons: BoxLink[] = [],
     public buttons: BoxLink[] = [],
     public link: BoxLink = null
