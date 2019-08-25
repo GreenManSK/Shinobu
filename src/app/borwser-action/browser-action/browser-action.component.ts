51921cc7b056a3d4b0972374e0a61585bc1c8da7
Lukáš Kurčík <lukas.kurcik@gmail.com>
2019-08-25 17:02:29 +0200

    BrowserAction uses parsers

--- src/app/borwser-action/browser-action/browser-action.component.ts
+++ src/app/borwser-action/browser-action/browser-action.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AnidbSongParserService } from '../../service/parsers/anidb-song-parser.service';
+import { SiteParser } from '../../service/parsers/site-parser';
+import { PopUpService } from '../../services/pop-up.service';
 
 @Component({
   selector: 'app-browser-action',
@@ -8,14 +10,37 @@ import { AnidbSongParserService } from '../../service/parsers/anidb-song-parser.
 })
 export class BrowserActionComponent implements OnInit {
 
-  constructor(
-    private anidbSongParser: AnidbSongParserService
-  ) {
+  public static readonly WIDTH = 600;
+  public static readonly HEIGHT = 500;
 
-    anidbSongParser.getData('http://anidb.net/song/102177').then(console.log);
+  private parsers: SiteParser[];
+
+  constructor(
+    public popUpService: PopUpService,
+    anidbSongParser: AnidbSongParserService
+  ) {
+    this.parsers = [
+      anidbSongParser
+    ];
   }
 
   ngOnInit() {
+    chrome.tabs.query({currentWindow: true, active: true}, ( tabs ) => {
+      const url = tabs[0].url;
+      for (const parser of this.parsers) {
+        if (parser.match(url)) {
+          parser.getData(url).then(( data ) => {
+            this.popUpService.openPopUp(
+              parser.getFormUrl(data),
+              '',
+              BrowserActionComponent.WIDTH,
+              BrowserActionComponent.HEIGHT
+            );
+          });
+          break;
+        }
+      }
+    });
   }
 
 }
