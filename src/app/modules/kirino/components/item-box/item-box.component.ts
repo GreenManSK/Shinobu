7d923214449bd294aba6d47f89e9700260043efb
Lukáš Kurčík <lukas.kurcik@gmail.com>
2019-08-31 19:59:24 +0200

    Fixed item-box sorting

--- src/app/modules/kirino/components/item-box/item-box.component.ts
+++ src/app/modules/kirino/components/item-box/item-box.component.ts
@@ -79,7 +79,7 @@ export class ItemBoxComponent implements OnInit {
   private prepareRenderedItems(): void {
     this.hiddenGroups = {};
     this.renderedItems = [...this._items]
-      .sort(( a, b ) => a.date.getTime() - b.date.getTime())
+      .sort(( a, b ) => (a.date ? a.date.getTime() : 0) - (b.date ? b.date.getTime() : 0))
       .filter(i => {
         if (!this.hiddenKeys.has(i.groupKey)) {
           return true;
