ff71bf8e7b61241b5f274b99794e933192a51481
Lukáš Kurčík <lukas.kurcik@gmail.com>
2022-05-01 15:22:55 +0200

    Add firebase to project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Shinobu
+
+## Setup
+1. Create `firebase.json` with configuration for firebase
--- ShinobuV4/src/environments/environment.prod.ts
+++ ShinobuV4/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'shinobu-7c893',
+    appId: '1:826493978428:web:365398e02614528cee0039',
+    storageBucket: 'shinobu-7c893.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAviL8dFBP8s6kmgU4403MulODzSaMuMg4',
+    authDomain: 'shinobu-7c893.firebaseapp.com',
+    messagingSenderId: '826493978428',
+  },
   production: true
 };
