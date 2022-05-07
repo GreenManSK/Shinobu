e4faa24e2caadbfa407c9b0e51d39a1c2034771d
Lukáš Kurčík <lukas.kurcik@gmail.com>
2022-05-07 22:26:12 +0200

    Adding note

--- ShinobuV4/src/app/components/shinobu/notes/notes.component.ts
+++ ShinobuV4/src/app/components/shinobu/notes/notes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Note } from '../../../data/shinobu/Note';
+import { NoteService } from '../../../services/data/shinobu/note.service';
+import { LocalPreferenceService } from '../../../services/data/local-preference.service';
 
 @Component({
   selector: 'notes',
@@ -8,11 +10,42 @@ import { Note } from '../../../data/shinobu/Note';
 })
 export class NotesComponent implements OnInit {
 
-  public activeNote = new Note("title", "text");
+  private static readonly ACTIVE_NOTE_KEY = 'activeNote';
 
-  constructor() { }
+  public notes: Note[] = [];
+  public activeNote?: Note;
 
-  ngOnInit(): void {
+  constructor( private noteService: NoteService, private localPreferenceService: LocalPreferenceService ) {
   }
 
+  ngOnInit(): void {
+    this.noteService.onReady().then(() => this.prepareNotes());
+  }
+
+  private prepareNotes() {
+    this.noteService.getAll().subscribe(notes => {
+      this.notes = notes;
+      if (this.notes.length <= 0) {
+        this.createNote();
+        return;
+      }
+      const activeNoteId = this.localPreferenceService.get(NotesComponent.ACTIVE_NOTE_KEY, 0);
+      const activeNoteCandidate = this.notes.filter(note => note.id === activeNoteId);
+      this.setActiveNote(activeNoteCandidate.length > 0 ? activeNoteCandidate[0] : this.notes[0]);
+    });
+  }
+
+  private setActiveNote( note: Note ) {
+    this.localPreferenceService.save(NotesComponent.ACTIVE_NOTE_KEY, note.id);
+    this.activeNote = note;
+  }
+
+  private createNote() {
+    const note = new Note('New note ' + Date.now());
+    return this.noteService.save(note);
+  }
+
+  public addNote() {
+    this.createNote().then(note => this.setActiveNote(note));
+  }
 }
