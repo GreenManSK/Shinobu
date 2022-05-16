7e246669c647c490e1040c87a087b0c8c400ac42
Lukáš Kurčík <lukas.kurcik@gmail.com>
2022-05-16 22:25:52 +0200

    Fix url for chrome extension to open

--- chrome/action.js
+++ chrome/action.js
@@ -1,1 +1,1 @@
-window.open(`http://localhost:4200/#/browser-action?url=${encodeURIComponent(window.location)}`, '_blank');
+window.open(`https://shinobu.kurcik.sk/#/browser-action?url=${encodeURIComponent(window.location)}`, '_blank');
