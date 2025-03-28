5e10fa79dc7188c702697db6fc48247f3a12eded
Lukáš Kurčík <lukas.kurcik@gmail.com>
2025-03-28 17:47:56 +0100

    eslint

--- cors-skip-server/index.js
+++ cors-skip-server/index.js
@@ -1,35 +1,37 @@
-const host = process.argv.length > 3 ? process.argv[3] : "127.0.0.1";
-const port = process.argv.length > 2 ? process.argv[2] : "6446";
+const host = process.argv.length > 3 ? process.argv[3] : '127.0.0.1';
+const port = process.argv.length > 2 ? process.argv[2] : '6446';
 
 const allowedUrls = [
-    "anison.info/data/song/",
-    "anidb.net/anime/",
-    "anidb.net/song/",
-    "anidb.net/episode/",
-    "www.thetvdb.com/series/",
-    "thetvdb.com/series/",
-    "www.amazon.co.jp/gp/product/",
-    "www.amazon.co.jp/kindle-dbs/productPage/ajax/",
-    "api.anidb.net:9001"
-]
+    'anison.info/data/song/',
+    'anidb.net/anime/',
+    'anidb.net/song/',
+    'anidb.net/episode/',
+    'www.thetvdb.com/series/',
+    'thetvdb.com/series/',
+    'www.amazon.co.jp/gp/product/',
+    'www.amazon.co.jp/kindle-dbs/productPage/ajax/',
+    'api.anidb.net:9001',
+];
 
 const cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: [],
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2'],
-    handleInitialRequest: (req, res, location) => {
-        if (!location) {
-            return false;
-        }
-        const url = `${location.host}${location.path}`;
-        for (const allowed of allowedUrls) {
-            if (url.startsWith(allowed)) {
+cors_proxy
+    .createServer({
+        originWhitelist: [],
+        requireHeader: ['origin', 'x-requested-with'],
+        removeHeaders: ['cookie', 'cookie2'],
+        handleInitialRequest: (req, res, location) => {
+            if (!location) {
                 return false;
             }
-        }
-        return true;
-    }
-}).listen(port, host, () => {
-    console.log(`Running CORS aAnywhere on ${host}:${port}`);
-});
+            const url = `${location.host}${location.path}`;
+            for (const allowed of allowedUrls) {
+                if (url.startsWith(allowed)) {
+                    return false;
+                }
+            }
+            return true;
+        },
+    })
+    .listen(port, host, () => {
+        console.log(`Running CORS aAnywhere on ${host}:${port}`);
+    });
--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,36 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import {defineConfig, globalIgnores} from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default defineConfig([
+    globalIgnores(['cors-skip-server/*']),
+    {
+        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+        plugins: {js},
+        extends: ['js/recommended'],
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+    tseslint.configs.recommended,
+    eslintPluginPrettierRecommended,
+    {
+        files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+        plugins: {
+            react,
+        },
+        languageOptions: {
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+            globals: {
+                ...globals.browser,
+            },
+        },
+        rules: {
+            'prettier/prettier': 'error',
+        },
     },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+]);
