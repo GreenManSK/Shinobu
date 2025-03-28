fa216fcc26cf5698a1df513789b5d2a255a9ef69
Lukáš Kurčík <lukas.kurcik@gmail.com>
2025-03-28 16:08:10 +0100

    Revert "Local storage support"
    This reverts commit a5229cfa4964d10fc0643eb6551ce67286cd1713.

--- shinobu-v5/src/App.tsx
+++ shinobu-v5/src/App.tsx
@@ -1,50 +1,25 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { LocalStorageProvider } from "./storage/local-storage-provider";
-import { useAllData, useMutateData } from "./storage/data-hooks";
-
-function Content() {
-  const data = useAllData<{ id?: string; name: string }>("test");
-  const { remove } = useMutateData<{ id?: string; name: string }>("test");
-  return (
-    <ul>
-      {data.map((item) => (
-        <li key={item.id}>
-          {item.id}
-          {item.name}
-          <button onClick={() => remove(item)}>Delete</button>
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-function AddContentButton() {
-  const { save } = useMutateData<{ id?: string; name: string }>("test");
-  return (
-    <button
-      onClick={() => {
-        save({ name: "New Item" });
-      }}
-    >
-      Add
-    </button>
-  );
-}
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <LocalStorageProvider>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Content />
-          <AddContentButton />
-          <Content />
-        </header>
-      </div>
-    </LocalStorageProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
